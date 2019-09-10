dc2577bfd622a916a10c275902df10aac45e306e
Jonathan.e <jonathanerquinigo@pursuit.org>
2019-09-10 16:37:20 -0400

    working on backend auth

---
+++ backend/db/queries/usersQueries.js
@@ -0,0 +1,2 @@
+const { db } = require("../index.js");
+const authHelpers = require("../../auth/helpers");
---
+++ backend/routes/sessions.js
@@ -0,0 +1,15 @@
+var express = require("express");
+var router = express.Router();
+const passport = require("../auth/local");
+const { loginRequired } = require("../auth/helpers");
+const {
+  loginUser,
+  isLoggedIn,
+  logoutUser
+} = require("../db/queries/sessionsQueries.js");
+
+router.post("/login", passport.authenticate("local", {}), loginUser);
+router.get("/isLoggedIn", isLoggedIn);
+router.post("/logout", loginRequired, logoutUser);
+
+module.exports = router;
